d2f8eaf375ca0d7140ddd464865d0c0f870ecc16
Hemanto das <mehemanto@gmail.com>
2019-07-03 17:37:32 +0530

    add main_app and routing and templer letterinh

--- Treasuregram/main_app/views.py
+++ Treasuregram/main_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    name = 'gold'
+    value = '1233'
+    context = {
+        'world': name,
+        'page': value
+    }
+    return render(request, 'index.html', context)
